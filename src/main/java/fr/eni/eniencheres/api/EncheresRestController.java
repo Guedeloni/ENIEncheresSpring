13b3d1b0726cbb7cba17f50ff346244f08855fd7
Guenole QUIDEAU <guenole.quideau2022@campus-eni.fr>
2022-08-23 14:32:53 +0200

    validation encheres + getEncheresByArticleId

--- src/main/java/fr/eni/eniencheres/api/EncheresRestController.java
+++ src/main/java/fr/eni/eniencheres/api/EncheresRestController.java
@@ -32,28 +32,50 @@ public class EncheresRestController {
     @GetMapping
     public List<Enchere> getlistEnchere(){return enchereService.listEnchere();}
 
-    @GetMapping("{id}")
-    public Enchere getEnchereById(@PathVariable long id) {return enchereService.getEnchereById(id);}
-
+    @GetMapping("{articleId}")
+    public List<Enchere> getEnchereByArticleId(@PathVariable long articleId) {
+        Article article = articleService.getArticleById(articleId);
+        return article.getEnchereList();
+    }
 
     @PostMapping("{articleId}/{utilisateurId}")
     public Enchere postEnchere(@PathVariable long articleId, @PathVariable long utilisateurId, @RequestBody Enchere enchere) throws ENIEncheresException {
 
+        Article article = articleService.getArticleById(articleId);
+
         // L'utilisateur ne doit pas etre le vendeur :
         // article ne doit pas etre ds. la liste des articles "vendus" (ou en cours) de l'utilisateur
         List<Article> articleList = utilisateurService.getUtilisateurById(utilisateurId).getArticleVenduList();
-        if (articleList.contains(articleService.getArticleById(articleId))) {
+        if (articleList.contains(article)) {
             throw new ENIEncheresException(Message.VENDEUR_NON_AUTORISE.showMsg());
         }
 
-        Article article = articleService.getArticleById(articleId);
-        enchere.setArticle(article);
+        // Le montant doit etre superieur au prix initial (liste d'enchere de l'article vide)
+        List<Enchere> enchereListFromArticle = article.getEnchereList();
+        if (enchereListFromArticle.isEmpty()) {
+            if (enchere.getMontantEnchere() <= article.getPrixInitial()) {
+                throw new ENIEncheresException(Message.MONTANT_ENCHERE_INSUFFISANT.showMsg());
+            }
+        // ni a la derniere enchere
+        } else if (enchere.getMontantEnchere() <=
+                    enchereListFromArticle.get(enchereListFromArticle.size()-1).getMontantEnchere()) {
+                throw new ENIEncheresException(Message.MONTANT_ENCHERE_INSUFFISANT.showMsg());
+        }
 
+        // Ajout article a enchere et inversement
+        enchere.setArticle(article);
+        enchereListFromArticle.add(enchere);
+
+        // Ajout utilisateur a enchere et inversement
         Utilisateur utilisateur = utilisateurService.getUtilisateurById(utilisateurId);
         enchere.setUtilisateur(utilisateur);
+        List<Enchere> enchereListFromUtilisateur = utilisateur.getEnchereList();
+        enchereListFromUtilisateur.add(enchere);
 
+        // Initialisation date enchere a la date du jour
         enchere.setDateEnchere(LocalDate.now());
 
+        // Enregistrement enchere en DB
         enchereService.addEnchere(enchere);
         return enchere;
     }
--- src/main/java/fr/eni/eniencheres/util/Message.java
+++ src/main/java/fr/eni/eniencheres/util/Message.java
@@ -1,7 +1,8 @@
 package fr.eni.eniencheres.util;
 
 public enum Message {
-    VENDEUR_NON_AUTORISE("Opération non autorisée pour le vendeur");
+    VENDEUR_NON_AUTORISE("Opération non autorisée pour le vendeur"),
+    MONTANT_ENCHERE_INSUFFISANT("Enchérissement inférieur ou égal au dernier montant");
 
     private String msg;
     Message(String msg) {
