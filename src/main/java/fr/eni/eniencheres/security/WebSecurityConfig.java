308ff7d3bd987374aa99bc1c6abbbace877c0c99
Thomas Chalanson <thomas.chalanson@sfr.fr>
2022-08-23 14:33:46 +0200

    do the login security

--- src/main/java/fr/eni/eniencheres/security/WebSecurityConfig.java
+++ src/main/java/fr/eni/eniencheres/security/WebSecurityConfig.java
@@ -1,42 +1,49 @@
 package fr.eni.eniencheres.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
-
 public class WebSecurityConfig {
-
-
+    @Autowired
+    private JwtFilter jwtFilter;
 
     @Bean // On définit un bean pour la configuration des routes
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        /**** Sur quels chemins impose-t-on d'être authentifié ****/
-        http.authorizeRequests()
-                .antMatchers("/admin/**").hasRole("admin") // 1 - Si route /admin/*** => besoin d'avoir le rôle admin
-                .antMatchers("/prive/**").authenticated() // 2 - Sinon, si route /prive/*** => besoin d'être authentifie
-                .anyRequest().permitAll().and() 		// 3 - Sinon, on autorise les autres requêtes
-                /**** On précise qu'on souhaite une authentification username/password ****/
-                .formLogin();
-        // .loginPage("/login"); si on veut avoir une page personalisée de login
-
-        // permet d'éviter les erreurs 403 sur les requêtes POST d'API
-        // pas idéal => normalement il faudrait gérer des json web token avec l'api
-        http.csrf().disable();
-
+        http.authorizeRequests() // ON AUTORISE
+                .antMatchers(HttpMethod.GET, "/**").permitAll() // les requêtes GET
+                .antMatchers(HttpMethod.OPTIONS, "/api/**").permitAll() // les requêtes OPTIONS
+                .antMatchers("/api/login").permitAll() // les requêtes pour se loguer
+                // sinon, besoin d'être authentifie (c'est fait au niveau du filtre avec : SecurityContextHolder.getContext().setAuthentication(authenticationToken);)
+                .anyRequest().authenticated()
+                .and().csrf().ignoringAntMatchers("/api/**");
+        //.formLogin(); commenté car on ne veut pas de redirection vers le formulaire de login
+        //.formLogin(); => fotmulaire par défault de Spring ! si pas de LoginRestController
+        /*****************************************************************
+         * AVANT DE FAIRE LA VERIFICATION DE SECURITE, ON AJOUTE UN FILTRE qui va
+         * vérifier la présence ou non d'un Json Web Token
+         ******************************************************************/
+        http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
-
-
-    @Bean // on définit un bean pour l'utilitaire d'encryption de mot de passe
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
-
-
 }
+
+	/*
+
+	@Bean // On définit un bean pour la configuration des routes
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http.authorizeRequests() // ON AUTORISE
+		.antMatchers(HttpMethod.GET, "/**").permitAll() // les requêtes GET
+		.antMatchers(HttpMethod.OPTIONS, "/api/**").permitAll() // les requêtes OPTIONS
+		.antMatchers("/api/login").permitAll() // les requêtes pour se loguer
+		.anyRequest().authenticated() // sinon, besoin d'être authentifie
+		.and().csrf().ignoringAntMatchers("/api/**");
+	//.formLogin(); commenté car on ne veut pas de redirection vers le formulaire de login
+
+	 */
