a47e9c8375d36d160dd879ff0877e7f2f3ae4056
Guenole QUIDEAU <guenole.quideau2022@campus-eni.fr>
2022-08-24 16:05:12 +0200

    Create Article with currentUser OK

--- src/main/java/fr/eni/eniencheres/util/Message.java
+++ src/main/java/fr/eni/eniencheres/util/Message.java
@@ -2,6 +2,7 @@ package fr.eni.eniencheres.util;
 
 public enum Message {
     VENDEUR_NON_AUTORISE("Opération non autorisée pour le vendeur"),
+    PB_CREATION_ARTICLE("Problème lors de la création de l'article"),
     MONTANT_ENCHERE_INSUFFISANT("Enchérissement inférieur ou égal au dernier montant");
 
     private String msg;
